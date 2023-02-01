7f6fad26d8ee395b301829c1fa2e1374ed2b081c
sanzaramanturov <sanjar.mix18@gmail.com>
2023-02-01 07:04:45 -0600

    h

---
+++ src/Car.java
@@ -0,0 +1,15 @@
+public class Car {
+    int year;
+    String brend;
+    String color;
+
+
+    public static void gaz(){
+        System.out.println("Mashina jurdu");
+
+    }
+    public static void tormoz (){
+        System.out.println("Mashina toktodu");
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+
+     /*Car car = new Car ();
+     car.brend = "Tesla";
+     car.color = "Blue";
+     car.year = 2023;
+
+
+     Car car1 = new Car ();
+     car1.brend = "Rolls Roys ";
+     car1.color = "Bleak";
+     car1.year = 2023;
+
+        System.out.println("My first car is " + car.brend + " color is " + car.color + " Year is " + car.year);
+        System.out.println("My secend car is " + car1.brend);*/
+
+         Car tesla = new Car ();
+
+         tesla.brend = "Modal X";
+         tesla.color = "Purple";
+         tesla.year = 2023;
+        System.out.println(tesla.brend + " " + tesla.color + " " + tesla.year);
+
+
+    }
+}
